293664b1f2d19d04bbae652bb92900f2873e1f30
Motojima0410 <motoshu@icloud.com>
2026-04-29 21:49:17 +0900

    課題2の構成修正およびSpring Boot形式への移行
    課題2の構成修正を実施し、Spring Boot形式に移行しました。
    HTML/CSS/JSをresources/static配下に配置しました。
    ご確認のほどお願いします。

---
+++ src/main/java/com/example/language_practical2/LanguagePractical2Application.java
@@ -0,0 +1,13 @@
+package com.example.language_practical2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguagePractical2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguagePractical2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/language_practical2/LanguagePractical2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.language_practical2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguagePractical2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
